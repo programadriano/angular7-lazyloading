a81d8b5ca8c6cef57d770ca2a2fc9ff4c355670e
Thiago Adriano <thiago.adriano26@gmail.com>
2018-11-12 22:07:35 -0200

    add router default

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes =
-  [{ path: 'home', loadChildren: './home/home.module#HomeModule' },
-  { path: 'login', loadChildren: './login/login.module#LoginModule' }
-  ];
+const routes: Routes = [
+  { path: 'home', loadChildren: './home/home.module#HomeModule' },
+  { path: 'login', loadChildren: './login/login.module#LoginModule' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
